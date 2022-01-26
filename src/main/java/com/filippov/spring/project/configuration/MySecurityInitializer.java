e63bfa291871ca040cdac0855f8c821afff66ed7
Pavel Filippov <p.filippov2.20@gmail.com>
2022-01-26 14:11:42 +0300

    checkBox+Select+ Security do not work yet and validations(

---
+++ src/main/java/com/filippov/spring/project/configuration/MySecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.filippov.spring.project.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
--- src/main/java/com/filippov/spring/project/controller/CarController.java
+++ src/main/java/com/filippov/spring/project/controller/CarController.java
@@ -4,6 +4,7 @@ import com.filippov.spring.project.entity.Car;
 import com.filippov.spring.project.service.CarService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @Controller
+
 public class CarController {
     @Autowired
     private CarService carService;
