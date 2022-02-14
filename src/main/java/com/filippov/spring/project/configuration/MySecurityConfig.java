56b8eb11cbd238b8403accbfb5dd590e82bea191
Pavel Filippov <p.filippov2.20@gmail.com>
2022-02-14 23:48:54 +0300

    checkBox+Select+ Security do not work yet and validations((

--- src/main/java/com/filippov/spring/project/configuration/MySecurityConfig.java
+++ src/main/java/com/filippov/spring/project/configuration/MySecurityConfig.java
@@ -1,21 +1,39 @@
-//package com.filippov.spring.project.configuration;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.User.UserBuilder;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//
-//@EnableWebSecurity
-//public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+package com.filippov.spring.project.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        // add our users for in memory authentication
+// не выходит MVC на авторизацию!!!!!(((( ПОЧЕМУ!!! Пробовал через таблицы, и хардкодил.
+        UserBuilder users = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(users.username("user").password("test123").roles("USER"))
+                .withUser(users.username("manager").password("test123").roles("MANAGER"))
+                .withUser(users.username("admin").password("test123").roles("ADMIN"));
+    }
+
+
+
 //    @Override
 //    protected void configure(HttpSecurity http) throws Exception {
+//
+//
 //        http
 //                .authorizeRequests()
 //                .antMatchers("/").permitAll()
@@ -30,6 +48,7 @@
 //                .and()
 //                .csrf().disable();
 //    }
+//
 //    @Bean
 //    @Override
 //    public UserDetailsService userDetailsService() {
@@ -42,22 +61,15 @@
 //
 //        return new InMemoryUserDetailsManager(user);
 //    }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-////        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-////        auth.inMemoryAuthentication()
-////                .withUser(userBuilder
-////                        .username("pavel").password("pavel")
-////                        .roles("ADMIN"))
-////                .withUser(userBuilder
-////                        .username("user").password("user")
-////                        .roles("USER"));
-//
-//    }
+
+
+//        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+//        auth.inMemoryAuthentication()
+//                .withUser(userBuilder
+//                        .username("pavel").password("pavel")
+//                        .roles("ADMIN"))
+//                .withUser(userBuilder
+//                        .username("user").password("user")
+//                        .roles("USER"));
+
+}
